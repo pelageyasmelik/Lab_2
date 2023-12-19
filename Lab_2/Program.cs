fa15d30e857b8c1ee9249862e8b81ef114bdef75
GlankS21 <asgar21502@gmail.com>
2023-12-20 01:07:53 +0300

    update

--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -4,6 +4,6 @@ using Lab_2.View;
 public class Program {
     public static void Main(String[] args) {
         View v = new View(new dictionary());
-        v.start();
+        v.start(); 
     }
 }
